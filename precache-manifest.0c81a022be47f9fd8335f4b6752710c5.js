f9c923d86d2f614c091814c7d42411fce0e41526
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2018-11-20 09:21:08 -0700

    Updates

---
+++ precache-manifest.0c81a022be47f9fd8335f4b6752710c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends4/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "bbe5ee24924b58ae53ba",
+    "url": "/robofriends4/static/js/runtime~main.bbe5ee24.js"
+  },
+  {
+    "revision": "b91319c91f3fb8a63ff1",
+    "url": "/robofriends4/static/js/main.b91319c9.chunk.js"
+  },
+  {
+    "revision": "6e18960d8754ebdc1ee8",
+    "url": "/robofriends4/static/js/1.6e18960d.chunk.js"
+  },
+  {
+    "revision": "b91319c91f3fb8a63ff1",
+    "url": "/robofriends4/static/css/main.633d2f19.chunk.css"
+  },
+  {
+    "revision": "6e18960d8754ebdc1ee8",
+    "url": "/robofriends4/static/css/1.940b7dba.chunk.css"
+  },
+  {
+    "revision": "505aa43ffa7158bd709d340a298cf02d",
+    "url": "/robofriends4/index.html"
+  }
+];
